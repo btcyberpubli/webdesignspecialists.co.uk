d0b0ad993e8f9e985b79aea910df2f8432d0cefc
btcyberpubli <btcyberpubli@gmail.com>
2026-01-30 15:00:56 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+// Web Design Specialists Ltd. - Configuration
+const businessConfig = {
+  businessName: 'Web Design Specialists Ltd.',
+  domain: 'webdesignspecialists.co.uk',
+  phone: '+44 161 3452 789',
+  email: 'contact@webdesignspecialists.co.uk',
+  businessHours: 'Monday - Friday, 09:30 - 18:00',
+  businessAddress: {
+    street: '3rd Floor, Tech Hub',
+    streetNumber: '18 King Street',
+    city: 'Manchester',
+    postcode: 'M2 6AG',
+    county: 'Greater Manchester',
+    country: 'United Kingdom'
+  },
+  businessType: 'Web Design and Development Services',
+  registrationNumber: 'Company Registration: 15432891',
+  colorPalette: {
+    primary: '#1F3C88',
+    secondary: '#4DA3FF',
+    white: '#FFFFFF'
+  },
+  operatingYears: '2012-present'
+};
